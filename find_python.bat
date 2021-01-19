54e268f588353a7eb7506ef631e1a4e885856f75
y-masutani <masutani@isc.osakac.ac.jp>
2021-01-19 12:42:00 +0900

    インストールの際にPython 3.8も探すようにした．

--- find_python.bat
+++ find_python.bat
@@ -1,7 +1,7 @@
 @echo off
 
 setlocal
-set PYTHON_VER=37
+set PYTHON_VER=38
 set FIND_DIRS=%~d0\local "%ProgramFiles%" "%ProgramFiles(x86)%" %~d0
 
 FOR  %%a in (%FIND_DIRS% "%ProgramFiles%" ) do (
@@ -10,6 +10,16 @@ FOR  %%a in (%FIND_DIRS% "%ProgramFiles%" ) do (
     goto :end
   )
 )
+
+set PYTHON_VER=37
+
+FOR  %%a in (%FIND_DIRS% "%ProgramFiles%" ) do (
+  if exist %%a\Python%PYTHON_VER%\python.exe (
+    echo %%a\Python%PYTHON_VER%
+    goto :end
+  )
+)
+
 :end
 endlocal
 echo on
